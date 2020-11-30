1db4f3e5dc78272e0cd7876240d3feb46c7e1989
refael-farjun <refaelfarjun5@gmail.com>
2020-11-30 15:50:52 +0200

    add classes..

--- src/test/java/bgu/spl/mics/SimpleMicroService.java
+++ src/test/java/bgu/spl/mics/SimpleMicroService.java
@@ -2,7 +2,7 @@ package bgu.spl.mics;
 
 public class SimpleMicroService extends MicroService {
     /**
-     * @param name the micro-service name (used mainly for debugging purposes -
+     *  the micro-service name (used mainly for debugging purposes -
      *             does not have to be unique)
      */
     public SimpleMicroService() {
